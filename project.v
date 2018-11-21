85584cd3c65f8b2037b461cc27c20701144fc859
will <will.qie@mail.utoronto.ca>
2018-11-21 00:35:50 -0500

    finishing traffic

--- project.v
+++ project.v
@@ -67,6 +67,7 @@ module project
 	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
 	// for the VGA controller, in addition to any other functionality your design may require.
     wire counter_reset, count_complete, incr_x, incr_y, load;
+    wire [3:0] xpos;
     
     trafficData td(
         .fastclock(CLOCK_50),
@@ -76,10 +77,11 @@ module project
         .colour(colour),
         .colourIn(3'b110),
         .incr_x(incr_x),
-        .incr_y(~KEY[3]),
+        .incr_y(incr_y),
         .load(load),
         .count_complete(count_complete),
-        .counter_reset(counter_reset)
+        .counter_reset(counter_reset),
+        .xpos(xpos)
     );
 
     trafficControl tc(
@@ -90,7 +92,8 @@ module project
         .incr_x(incr_x),
         .incr_y(incr_y),
         .load(load),
-        .writeEn(writeEn)
+        .writeEn(writeEn),
+        .xpos(xpos)
     );
 
 endmodule
@@ -230,10 +233,10 @@ up, down, resetn, counter_reset, count_complete, erase, writeEn, load);
 endmodule
 
 module trafficData(fastclock, resetn, x, y, colour, colourIn, incr_x, incr_y, load
-, count_complete, counter_reset);
+, count_complete, counter_reset, xpos);
     input fastclock, resetn, load, counter_reset, incr_x, incr_y;
     input [2:0] colourIn;
-    reg [3:0] xpos;
+    output reg [3:0] xpos;
     reg [1:0] ypos;
     output [7:0] x;
     output [6:0] y;
@@ -251,28 +254,28 @@ module trafficData(fastclock, resetn, x, y, colour, colourIn, incr_x, incr_y, lo
     shiftRegister line0(
         .clock(clock),
         .q(q0),
-        .init_val(16'b0000_1111_0000_0000), // Pseudo-random
+        .init_val(16'b1000_1101_1000_0000), // Pseudo-random
         .resetn(resetn)
     );
 
     shiftRegister line1(
         .clock(clock),
         .q(q1),
-        .init_val(16'b1100_0011_0001_0010),
+        .init_val(16'b1100_0011_0001_0011),
         .resetn(resetn)
     );
 
     shiftRegister line2(
         .clock(clock),
         .q(q2),
-        .init_val(16'b0011_0011_0001_1011),
+        .init_val(16'b0011_0111_0001_1001),
         .resetn(resetn)
     );
 
     shiftRegister line3(
         .clock(clock),
         .q(q3),
-        .init_val(16'b0110_0011_1001_1000),
+        .init_val(16'b0110_0011_1001_1100),
         .resetn(resetn)
     );
 
@@ -287,30 +290,30 @@ module trafficData(fastclock, resetn, x, y, colour, colourIn, incr_x, incr_y, lo
 		end
         if (load) begin
             xcoor <= xpos * 10;
-            ycoor <= ypos * 15;
+            ycoor <= (ypos + 1) * 15;
             if (ypos == 2'd0) begin
                 if (q0[4'd15 - xpos] == 1'b1)
                     colour <= colourIn;
                 else
-                    colour <= 3'b111;
+                    colour <= 3'b000;
             end
             if (ypos == 2'd1) begin
                 if (q1[4'd15-xpos] == 1'b1)
                     colour <= colourIn;
                 else
-                    colour <= 3'b111;
+                    colour <= 3'b000;
             end
             if (ypos == 2'd2) begin
-                if (q1[4'd15-xpos] == 1'b1)
+                if (q2[4'd15-xpos] == 1'b1)
                     colour <= colourIn;
                 else
-                    colour <= 3'b111;
+                    colour <= 3'b000;
             end
             if (ypos == 2'd3) begin
-                if (q1[4'd15-xpos] == 1'b1)
+                if (q3[4'd15-xpos] == 1'b1)
                     colour <= colourIn;
                 else
-                    colour <= 3'b111;
+                    colour <= 3'b000;
             end
         end
         else begin
@@ -330,24 +333,36 @@ module trafficData(fastclock, resetn, x, y, colour, colourIn, incr_x, incr_y, lo
 endmodule
 
 module trafficControl(fastclock, count_complete, counter_reset, resetn
-,incr_x, incr_y, load, writeEn);
+,incr_x, incr_y, load, writeEn, xpos);
     input fastclock, resetn, count_complete;
+    input [3:0] xpos;
     output reg counter_reset, load, incr_x, incr_y, writeEn;
 
     localparam s_load          = 4'd0,
                s_incr_x         = 4'd1,
                s_clear_counter  = 4'd2,
-               s_draw           = 4'd3;
+               s_draw           = 4'd3,
+               s_incr_y         = 4'd4,
+               s_inter          = 4'd5;
 
     reg [3:0] current_state, next_state;
-
+    
     always @(*)
     begin
         case (current_state)
             s_incr_x: next_state = s_load;
             s_load: next_state = s_clear_counter;
             s_clear_counter: next_state = s_draw;
-            s_draw: next_state = count_complete ? s_incr_x : s_draw;
+            s_draw: next_state = count_complete ? s_inter : s_draw;
+            s_inter: begin
+                if (xpos == 4'b1111) begin
+                    next_state = s_incr_y;
+                end
+                else begin
+                    next_state = s_incr_x;
+                end
+            end
+            s_incr_y: next_state = s_incr_x;
         endcase
     end
 
@@ -363,6 +378,7 @@ module trafficControl(fastclock, count_complete, counter_reset, resetn
             s_incr_x: incr_x = 1;
             s_clear_counter: counter_reset = 1;
             s_draw: writeEn = 1;
+            s_incr_y: incr_y = 1;
 			endcase
     end
 
@@ -418,136 +434,3 @@ module shiftRegister(clock, q, init_val, resetn);
             q <= {q[14:0], q[15]};
     end
 endmodule
-
-module hex(HEX, x);
-   input [3:0] x;
-   output [6: 0] HEX;
-
-   Hex0 h0(
-       .y(HEX[0]),
-       .c3(x[3]),
-       .c2(x[2]),
-       .c1(x[1]),
-       .c0(x[0])
-   );
-   Hex1 h1(
-       .y(HEX[1]),
-       .c3(x[3]),
-       .c2(x[2]),
-       .c1(x[1]),
-       .c0(x[0])
-   );
-   Hex2 h2(
-       .y(HEX[2]),
-       .c3(x[3]),
-       .c2(x[2]),
-       .c1(x[1]),
-       .c0(x[0])
-   );
-   Hex3 h3(
-       .y(HEX[3]),
-       .c3(x[3]),
-       .c2(x[2]),
-       .c1(x[1]),
-       .c0(x[0])
-   );
-   Hex4 h4(
-       .y(HEX[4]),
-       .c3(x[3]),
-       .c2(x[2]),
-       .c1(x[1]),
-       .c0(x[0])
-   );
-   Hex5 h5(
-       .y(HEX[5]),
-       .c3(x[3]),
-       .c2(x[2]),
-       .c1(x[1]),
-       .c0(x[0])
-   );
-   Hex6 h6(
-       .y(HEX[6]),
-       .c3(x[3]),
-       .c2(x[2]),
-       .c1(x[1]),
-       .c0(x[0])
-   );
-
-
-endmodule
-
-
-
-module Hex0(y, c3, c2, c1, c0);
-   output y;
-   input c3, c2, c1, c0;
-   assign y = ~((c1 & ~c0) | (~c3 & c1) | (c3 & ~c1 & ~c0) | (c0 & ~c3 & c2) | (~c2 & ~c1 & ~c0) | (c1 & c3 & c2) | (~c1 & c3 & ~c2));
-
-endmodule
-
-module Hex1(y, c3, c2, c1, c0);
-   output y;
-   input c3, c2, c1, c0;
-   assign y = ~((~c3 & ~c2) | ( ~c1 & ~c2) | (~c2 & c1 & ~c0) | (~c3 & c1 & c0) | (c3 & c0 & ~c1) | ( ~c3 & ~c1 & ~c0));
-
-endmodule
-
-module Hex2(y, c3, c2, c1, c0);
-   output y;
-   input c3, c2, c1, c0;
-   assign y = ~(~c1 & c0 | ~c3 & c2 | c3 & ~c2 | ~c1 & ~c3 | ~c3 & c0);
-endmodule
-
-module Hex3(y, c3, c2, c1, c0);
-   output y;
-   input c3, c2, c1, c0;
-   assign y = ~(c3 & ~c1 & ~c0 | ~c1 & c2 & c0 | c1 & ~c3 & ~c2 | c2 & c1 & ~c0 | ~c2 & c1 & c0 | ~c2 & ~c1 & ~c0);
-
-endmodule
-
-module Hex4(y, c3, c2, c1, c0);
-   output y;
-   input c3, c2, c1, c0;
-   assign y = ~(c1 & ~c0 | c3 & c2 | c1 & c3 | ~c2 & ~c1 & ~c0);
-
-endmodule
-
-module Hex5(y, c3, c2, c1, c0);
-   output y;
-   input c3, c2, c1, c0;
-   assign y =~( ~c1 & ~c0 | c3 & ~c2 | ~c0 & c2 & c1 | c1 & c2 & c3 | c2 & ~c1 & ~c3);
-
-endmodule
-
-module Hex6(y, c3, c2, c1, c0);
-   output y;
-   input c3, c2, c1, c0;
-   assign y = ~(c3 & ~c2 | c1 & ~c0 | c0 & c3 | ~c3 & c2 & ~c1 | c1 & ~c3 & ~c2);
-
-endmodule
-
-// module hex_decoder(hex_digit, segments);
-//     input [3:0] hex_digit;
-//     output reg [6:0] segments;
-   
-//     always @(*)
-//         case (hex_digit)
-//             4'h0: segments = 7'b100_0000;
-//             4'h1: segments = 7'b111_1001;
-//             4'h2: segments = 7'b010_0100;
-//             4'h3: segments = 7'b011_0000;
-//             4'h4: segments = 7'b001_1001;
-//             4'h5: segments = 7'b001_0010;
-//             4'h6: segments = 7'b000_0010;
-//             4'h7: segments = 7'b111_1000;
-//             4'h8: segments = 7'b000_0000;
-//             4'h9: segments = 7'b001_1000;
-//             4'hA: segments = 7'b000_1000;
-//             4'hB: segments = 7'b000_0011;
-//             4'hC: segments = 7'b100_0110;
-//             4'hD: segments = 7'b010_0001;
-//             4'hE: segments = 7'b000_0110;
-//             4'hF: segments = 7'b000_1110;   
-//             default: segments = 7'h7f;
-//         endcase
-// endmodule
